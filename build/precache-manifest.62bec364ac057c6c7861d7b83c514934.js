9187cbc3f031a35793db6fdeee42ed2ec35c0b48
Mahmoud Zakria <me@zakcoding.com>
2020-05-15 01:14:00 +0200

    removed build/ fron .gitignore

---
+++ build/precache-manifest.62bec364ac057c6c7861d7b83c514934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c245b8afda1f43c80ef791ff2d41b9d",
+    "url": "/albert-wiki/build/index.html"
+  },
+  {
+    "revision": "98e10d27507259328d25",
+    "url": "/albert-wiki/build/static/css/main.2875f180.chunk.css"
+  },
+  {
+    "revision": "3edf129ed578d15576a6",
+    "url": "/albert-wiki/build/static/js/2.2985415d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/albert-wiki/build/static/js/2.2985415d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e10d27507259328d25",
+    "url": "/albert-wiki/build/static/js/main.279f0aef.chunk.js"
+  },
+  {
+    "revision": "140390aec80a39482f96",
+    "url": "/albert-wiki/build/static/js/runtime-main.c1795548.js"
+  }
+]);
