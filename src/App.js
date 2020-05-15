94a205ce8e623d2a09ea2cf404cb5f1166f1a4ac
Mahmoud Zakria <me@zakcoding.com>
2020-05-15 06:45:33 +0200

    use of langlinks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Loader from './Components/Loader';
-import languages from './languages.js';
+import WikiBtns from './Components/Buttons.jsx';
+// import languages from './languages.js';
 
 function App() {
   const [preferredLang, setPreferredLang] = useState(navigator.language.split('-')[0]);
   const [wiki, setWiki] = useState('');
   const [isLoading, setIsLoading] = useState(true)
 
-  const reqURL = `https://${preferredLang}.wikipedia.org/w/api.php?action=parse&format=json&page=Albert_Einstein&prop=text%7Cimages&formatversion=1&origin=*`;
+  const reqURL = `https://${preferredLang}.wikipedia.org/w/api.php?action=parse&format=json&page=Albert_Einstein&prop=text%7Cimages%7Clanglinks&formatversion=1&origin=*`;
 
   useEffect(() => {
     const fetchWiki = async () => {
       const req = await fetch(reqURL);
       const res = await req.json();
-      setWiki(res.parse.text['*']);
+      setWiki(res.parse && res.parse.text ? res.parse.text['*'] : 'No page for this selected language');
       setIsLoading(false);
     }
     fetchWiki();
@@ -32,16 +33,7 @@ function App() {
         <h4>Choose your preferred language</h4>
       </header>
       <div className="App-btns">
-        {languages.length > 0 && languages.map(({ name, value }, i) =>
-          <button
-            id={name}
-            key={i}
-            className={`wiki-btn ${preferredLang === value ? 'selected' : ''}`}
-            disabled={preferredLang === value}
-            onClick={handleClick.bind(null, value)}>
-            {name}
-          </button>
-        )}
+        <WikiBtns handleClick={handleClick} preferredLang={preferredLang} />
       </div>
       <div className="wiki-wrapper">
         {isLoading ? <Loader /> : null}
---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+const langLinksURL = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Albert_Einstein&prop=langlinks&formatversion=1&origin=*';
+
+function Buttons({ preferredLang, handleClick }) {
+    const [languages, setLanguages] = useState([]);
+
+    useEffect(() => {
+        const fetchLangs = async () => {
+            const req = await fetch(langLinksURL);
+            const res = await req.json();
+            setLanguages(res.parse.langlinks);
+        }
+        fetchLangs();
+    })
+
+    return (
+        languages.length > 0 && languages.map(({ langname, lang }, i) =>
+            <button
+                id={langname}
+                key={i}
+                className={`wiki-btn ${preferredLang === lang ? 'selected' : ''}`}
+                disabled={preferredLang === lang}
+                onClick={handleClick.bind(null, lang)}>
+                {langname}
+            </button>
+        )
+
+    );
+}
+
+
+
+
+export default Buttons;
