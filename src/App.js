cfa6bfab5b77da8acb0a2e8eecf08c3004995ad4
Mahmoud Zakria <me@zakcoding.com>
2020-05-15 05:57:40 +0200

    added loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import Loader from './Components/Loader';
 import languages from './languages.js';
 
 function App() {
   const [preferredLang, setPreferredLang] = useState(navigator.language.split('-')[0]);
-  const [wiki, setWiki] = useState('Loading wiki...');
+  const [wiki, setWiki] = useState('');
+  const [isLoading, setIsLoading] = useState(true)
 
   const reqURL = `https://${preferredLang}.wikipedia.org/w/api.php?action=parse&format=json&page=Albert_Einstein&prop=text%7Cimages&formatversion=1&origin=*`;
 
@@ -13,9 +14,15 @@ function App() {
       const req = await fetch(reqURL);
       const res = await req.json();
       setWiki(res.parse.text['*']);
+      setIsLoading(false);
     }
     fetchWiki();
   })
+
+  const handleClick = (value) => {
+    setPreferredLang(value);
+    setIsLoading(true);
+  }
 
 
   return (
@@ -25,9 +32,21 @@ function App() {
         <h4>Choose your preferred language</h4>
       </header>
       <div className="App-btns">
-        {languages.length && languages.map(({ name, value }, i) => <button id={name} key={i} className={`wiki-btn ${preferredLang === value ? 'selected' : ''}`} onClick={() => setPreferredLang(value)}>{name}</button>)}
+        {languages.length > 0 && languages.map(({ name, value }, i) =>
+          <button
+            id={name}
+            key={i}
+            className={`wiki-btn ${preferredLang === value ? 'selected' : ''}`}
+            disabled={preferredLang === value}
+            onClick={handleClick.bind(null, value)}>
+            {name}
+          </button>
+        )}
       </div>
-      <div className="wiki-view" dangerouslySetInnerHTML={{ __html: wiki }}></div>
+      <div className="wiki-wrapper">
+        {isLoading ? <Loader /> : null}
+        <div className="wiki-view" dangerouslySetInnerHTML={!isLoading ? { __html: wiki } : null}></div>
+      </div>
     </div>
   );
 }
